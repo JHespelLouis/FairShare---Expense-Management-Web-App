582f2f9271722540e93775175576e314f862ac7d
Justin Hespel Louis <93406828+JHespelLouis@users.noreply.github.com>
2024-05-08 15:56:20 +0900

    removing old content

--- Backend-user-interface/Old/r_game.js
+++
@@ -1,9 +0,0 @@
-const userCtrl = require('./cl_game')
-const express = require("express");
-const router = express.Router();
-
-router.get('/:uid/:mid/:gid', userCtrl.getGame)
-router.get('/:uid/:mid', userCtrl.getGames)
-router.post('/', userCtrl.createGame)
-
-module.exports = router;
--- Backend-user-interface/Old/r_session.js
+++
@@ -1,8 +0,0 @@
-const userCtrl = require('./cl_session')
-const express = require("express");
-const router = express.Router();
-
-router.get('/:sessId', userCtrl.getSession)
-router.post('/', userCtrl.createSessions)
-
-module.exports = router;
